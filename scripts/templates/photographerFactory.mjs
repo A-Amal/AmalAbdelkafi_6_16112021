f3e52f9327ce64df12c98a3937925821da403a36
Abdelkafi Amal <zaghdanamal@yahoo.fr>
2021-12-14 12:16:59 +0100

    modifier fichier photographerfactory

--- scripts/templates/photographerFactory.mjs
+++ scripts/templates/photographerFactory.mjs
@@ -52,20 +52,19 @@ export function createMediaCardVideo(media){
     $wrapperMedia.classList.add('card');
     const photographerMediaPage =`
         <a href="assets/photographers/${media.photographerId}/${media.video}" class="card-media" aria-label=${media.title}, closeup view">
-           
             <video aria-label="${media.alt}" data-lightbox data-lightbox-caption="${media.title}">
                 <source src="assets/photographers/${media.photographerId}/${media.video}" type="video/mp4">
             </video>
             <i class="fas fa-video"></i>
-        
-            <div class="card-header">
-                <p class="card-header-title">${media.title}</p>
-                <div class="card-header-like" role="button" data-likes=${media.id}>
-                    <span class="counter" value="${media.likes}" data-likes-count>${media.likes}</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="likes"><path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"/></svg>
-                </div>
+        </a>
+        <div class="card-header">
+            <p class="card-header-title">${media.title}</p>
+            <div class="card-header-like" role="button" data-likes=${media.id}>
+                <span class="counter" value="${media.likes}" data-likes-count>${media.likes}</span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" aria-label="likes"><path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"/></svg>
             </div>
-        </a>`;
+        </div>
+        `;
     $wrapperMedia.innerHTML = photographerMediaPage;
     return ($wrapperMedia);
 }
